58dc5c722a26f4c12ff624f18bb1302bbecfde74
danchita45 <danchita45@gmail.com>
2023-02-01 08:32:52 -0600

    verificando

--- src/main/java/com/mycompany/mavenproject2/OBJ1.java
+++ src/main/java/com/mycompany/mavenproject2/OBJ1.java
@@ -9,5 +9,44 @@ package com.mycompany.mavenproject2;
  * @author danchita45
  */
 public class OBJ1 {
+    public int OBJ1ID;
+    public String Description;
+    public String Code;
+
+    public OBJ1(int OBJ1ID, String Description, String Code) {
+        this.OBJ1ID = OBJ1ID;
+        this.Description = Description;
+        this.Code = Code;
+    }
+
+    public OBJ1() {
+    }
+    
+
+    public int getOBJ1ID() {
+        return OBJ1ID;
+    }
+
+    public void setOBJ1ID(int OBJ1ID) {
+        this.OBJ1ID = OBJ1ID;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String Description) {
+        this.Description = Description;
+    }
+
+    public String getCode() {
+        return Code;
+    }
+
+    public void setCode(String Code) {
+        this.Code = Code;
+    }
+    
+    
     
 }
