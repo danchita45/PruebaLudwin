17b9980f1345a71f5a7b2fd3b3c4e8261e0aed5d
ludks1 <ludwindrts@gmail.com>
2023-01-31 19:46:05 -0600

    tercer commit

--- src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
+++ src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -1,7 +1,7 @@
 
 
 package com.mycompany.mavenproject2;
-
+import java.time.LocalDate;
 /**
  *
  * @author Danchita45
@@ -10,5 +10,20 @@ public class Mavenproject2 {
 
     public static void main(String[] args) {
         System.out.println("ola que pez mi acauamarino?!");
+        int temperatura = 0;
+            if (temperatura > 15) {
+          if (temperatura > 25) {
+          System.out.println("A la playa!!!");
+              } else {
+          System.out.println("A la montaña!!!");
+          }
+          } else if (temperatura < 5) {
+            boolean nevando = true;
+          if (nevando) {
+          System.out.println("A esquiar!!!");
+          }
+              } else {
+          System.out.println("A descansar... zZz");
+          }
     }
 }
